7d3b96abfc8cbfe562f6332e5da3d21ad71ea7c2
samgashyeong <samgashyeong@naver.com>
2021-07-31 23:11:14 +0900

    데이터를 리스트뷰화 시킴.

--- app/src/main/java/com/example/recyclerview/ListViewAdapter.java
+++ app/src/main/java/com/example/recyclerview/ListViewAdapter.java
@@ -1,15 +1,24 @@
 package com.example.recyclerview;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class ListViewAdapter extends BaseAdapter {
+    List<String> list;
+    public ListViewAdapter(List<String> list){
+        super();
+        this.list = list;
+    }
+
     @Override
     public int getCount() {
-        return 5;
+        return list.size();
     }
 
     @Override
@@ -24,8 +33,10 @@ public class ListViewAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        TextView text = new TextView(viewGroup.getContext());
-        text.setText(""+i);
-        return text;
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View itemView = inflater.inflate(R.layout.row_item, viewGroup, false);
+        TextView itemText = itemView.findViewById(R.id.itemTv);
+        itemText.setText(i);
+        return itemView;
     }
 }
